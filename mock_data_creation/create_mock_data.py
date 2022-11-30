2598a5c3f58c99f9dcc7986c33bb968f7649d3fb
Marcel <Marcel.Klotz@posteo.de>
2022-11-30 13:58:35 +0100

    Comments, clean up

--- mock_data_creation/create_mock_data.py
+++ mock_data_creation/create_mock_data.py
@@ -1,10 +1,11 @@
+# nice library to get mock data in controllable manner with quite some additional data types
+# see at https://faker.readthedocs.io/en/stable/index.html
 from faker import Faker as MockGen
-from faker.providers import internet
 import csv
-
 
 mock_data_generator = MockGen()
 
+# example data for the users table
 profile_fields = [ 'username', 'mail']
 profile_data = [ mock_data_generator.profile(profile_fields) for i in range(10)] # not tested/documented if that will give unique data
 
--- scripts/db/2_csv_seeds.sql
+++ scripts/db/2_csv_seeds.sql
@@ -1,4 +1,5 @@
 COPY users(name, email)
+-- while building the pg docker immge csv's from scripts/csv folder will be copied to 'docker-entry...' so adapt file names
 FROM '/docker-entrypoint-initdb.d/users.csv'
 DELIMITER ','
 CSV HEADER;
